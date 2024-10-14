e58134b1d484ca2d231a9b304ab7174ae6851698
maitysuman379 <maitysuman379@gmail.com>
2024-10-14 19:31:01 +0530

    newreact

--- React/src/App.js
+++ React/src/App.js
@@ -1,9 +1,19 @@
+import logo from './logo.svg';  
 import './App.css';
 
 function App() {
+  let name = 'Suman Maity'
   return (
     <>
-      <h1>Suman Maity</h1>
+      <nav>
+          <li className="liCss">Home</li>
+          <li className="liCss">About</li>
+          <li className="liCss">Contact</li>
+      </nav>
+      <div className="container">
+        <h1>Hello {name}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi vel similique quae! Nihil dolorum facilis quisquam labore et neque sequi, eos veniam illum fuga tempora itaque consequatur, in molestiae, impedit deleniti vitae magnam nemo ratione.</p>
+      </div>
     </>
   );
 }
